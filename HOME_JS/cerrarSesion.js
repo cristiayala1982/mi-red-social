a02789694d630543886fa3fa90b6c98c7bf78da0
cristian ayala <cristianayala10@hotmail.com>
2025-12-24 09:08:38 -0300

    Update cerrarSesion.js

--- HOME_JS/cerrarSesion.js
+++ HOME_JS/cerrarSesion.js
@@ -19,7 +19,7 @@ function configurarCerrarSesion() {
 
         // Redirigir al login después de unos segundos
         setTimeout(() => {
-          window.location.href = "login.html";
+          window.location.href = "index.html";
         }, 1500);
       }
     } catch (err) {
@@ -58,6 +58,7 @@ function mostrarNotificacion(mensaje) {
 
 // Ejecutar al cargar la página
 configurarCerrarSesion();
+
 
 
 
