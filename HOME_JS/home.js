8ef81fcc245c30eeb997a860a7bc097c3ec93340
cristian ayala <cristianayala10@hotmail.com>
2025-12-28 11:21:50 -0300

    Update home.js

--- HOME_JS/home.js
+++ HOME_JS/home.js
@@ -1,6 +1,7 @@
 // home.js
 import './buscador.js';
-import './navbar.js';
+//import './navbar.js'; creo que lo importaba dos veces
 
 
 // Si en el futuro agregás más módulos del home, los importás acá
+
